c9665f34660abf16eaa892c1f0cef24a37ed81f6
Aleksey Telitsa <aleha2552654@gmail.com>
2024-09-18 23:45:27 +0300

    refactor ace

--- game/deck.py
+++ game/deck.py
@@ -1,5 +1,7 @@
 import random
 import bot.database as db
+from config.language import GAME_RESULT, WIN_GAME, LOSE_GAME,DRAW_GAME
+
 def get_deck():
     deck_cards=[]
     for i in range(2,11):
@@ -38,31 +40,36 @@ def get_card(deck_cards):
     return card
 
 
-def summa_cards(mylist,user_score):
-    score=0
+def summa_cards(mylist, user_score):
+    score = 0
+    ace_count = 0
+    
     for card in mylist:
-        card=card.split()
-        if card[1]=="Валет" or card[1]=="Дама" or card[1]=="Король":
-            score+=10
-        elif card[1]=="Туз": # переделать механику подсчета очков
-            if user_score+11>21:
-                score+=1
-            else:
-                score+=11
-        else: 
-            score+=int(card[1])
-    return int(score)
+        card = card.split()
+        
+        if card[1] in ["Валет", "Дама", "Король"]:
+            score += 10
+        elif card[1] == "Туз":
+            ace_count += 1
+            score += 11
+        else:
+            score += int(card[1])
+
+    while score > 21 and ace_count:
+        score -= 10 
+        ace_count -= 1
+    
+    return score
 
 def calculate_result(player, user_id):
-    game_info = f"Карты дилера: {player.dealer_cards}\nСумма дилера: {player.dealer_score}\nВаши карты: {player.user_cards}\nСумма карт: {player.user_score}"
-
+    game_info=GAME_RESULT(player)
     if player.user_score > player.dealer_score and player.user_score < 22 or player.dealer_score > 21 and player.user_score < 22:
         db.add_win(user_id)
-        return f"Win,\n{game_info}"
+        return f"{WIN_GAME}\n\n{game_info}"
     elif player.user_score < player.dealer_score and player.dealer_score < 22 or player.user_score > 21 and player.dealer_score < 22:
         db.add_lose(user_id)
-        return f"lose,\n{game_info}"
+        return f"{LOSE_GAME}\n\n{game_info}"
     else:
         db.add_game(user_id)
-        return f'ничья,\n{game_info}'
+        return f'{DRAW_GAME}\n\n{game_info}'
 
