d7a1f452a81d9c819a5f6f51adb3209e3cf0304e
Aleksey Telitsa <aleha2552654@gmail.com>
2024-09-29 02:25:53 +0300

    Move initializing tables from middlewares

--- bot/__main__.py
+++ bot/__main__.py
@@ -5,9 +5,16 @@ import asyncio
 from bot.client import dp, bot
 from bot.handlers import routers
 from bot.middlewares import UserCheckMiddleware
+from database.users import create_user_table
+from database.supports import create_support_table
+from database.archive_support import create_archive_support_table
+create_user_table()
+create_support_table()
+create_archive_support_table()
 
 for router in routers:
     dp.include_router(router)
 dp.message.middleware.register(UserCheckMiddleware())
+
 print("Start polling")
 asyncio.run(dp.start_polling(bot))
